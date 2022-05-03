f2a07050e17cdf4742115d28d532cab81e7c3b81
Erdogan Alper <alper.e@unic.ac.cy>
2022-05-03 15:25:37 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            
+        }
+        public static int Sum(int a, int z)
+        {
+            return a * z;
+
+        }
+    }
+    
+}
